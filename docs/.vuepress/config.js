9808835ea5dfb30f85455355710fdbd79a7edac7
liujikuan <994249582@qq.com>
2022-01-07 14:50:57 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
             href: '/favicon.ico'
         }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
-    base: '/', // 这是部署到github相关的配置 下面会讲
+    base: '/', // 部署到github
     markdown: {
         lineNumbers: true // 代码块显示行号
     },
